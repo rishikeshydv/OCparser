9ae04260d010074a7663d60cfef556be8a4feb62
rishikeshydv <rishikeshadh4@gmail.com>
2024-01-23 17:46:11 +0545

    feat: token tracing added

--- ocparser/bin/json.ml
+++ ocparser/bin/json.ml
@@ -1,1 +1,9 @@
-type value = []
+type value = [  
+  | `Assoc of (string * value) list  
+  | `Bool of bool  
+  | `Float of float  
+  | `Int of int  
+  | `List of value list  
+  | `Null  
+  | `String of string
+]
